2153c7f3339cd8689291a5dfd4d2c30877c29bc6
AaMenaBo <aamenabo@est.utn.ac.cr>
2023-07-13 16:21:11 -0600

    Inicio de Parcial - Database

---
+++ src/DB/DB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DB;
+
+import Model.Cuenta;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author menab
+ */
+public class DB {
+    private static ArrayList<Cuenta> cuentas;
+
+    public ArrayList<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    
+    
+    
+}
